1ab4037b0d5c9db4b34d1c8e8cfc64de81f989cb
Hisaaki Sato <hisaaki.sato@speee.jp>
2019-03-19 21:14:35 +0900

    Add autoscaling adjust function

---
+++ functions/autoscaling-capacity-syncer/handler.rb
@@ -0,0 +1,34 @@
+require 'json'
+require 'aws-sdk-autoscaling'
+
+MAIN_REGION = 'ap-northeast-1'
+MAIN_GROUP = ENV['MAIN_GROUP']
+REGIONS = %w( ap-northeast-1 ap-southeast-2 )
+
+def lambda_handler(event:, context:)
+
+  autoscaling = Aws::AutoScaling::Client.new({ region: MAIN_REGION })
+  resp = autoscaling.describe_auto_scaling_groups({
+    auto_scaling_group_names: [ MAIN_GROUP ]
+  })
+  group = resp.auto_scaling_groups[0]
+  diff = group.desired_capacity - group.min_size
+  desired_capacity = group.desired_capacity
+
+  REGIONS.each do |region|
+    autoscaling = Aws::AutoScaling::Client.new({ region: region })
+    resp = autoscaling.describe_auto_scaling_groups({})
+
+    resp.auto_scaling_groups.each do |group|
+      capacity = diff == 0 ? group.min_size : desired_capacity
+      if capacity != group.desired_capacity
+        autoscaling.set_desired_capacity({
+          auto_scaling_group_name: group.auto_scaling_group_name,
+          desired_capacity: capacity
+        })
+        puts "Change desired capacity to #{capacity}: #{group.auto_scaling_group_name} (#{region})"
+      end
+    end
+  end
+  puts 'Done'
+end
