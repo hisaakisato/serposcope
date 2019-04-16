ba7ba6f3f25a7572622c628db81fb8f7883a3f77
Hisaaki Sato <hisaaki.sato@speee.jp>
2019-04-16 16:43:01 +0900

    Force to check alarm status

--- functions/autoscaling-capacity-syncer/handler.rb
+++ functions/autoscaling-capacity-syncer/handler.rb
@@ -1,8 +1,10 @@
 require 'json'
 require 'aws-sdk-autoscaling'
+require 'aws-sdk-cloudwatch'
 
 MAIN_REGION = 'ap-northeast-1'
 MAIN_GROUP = ENV['MAIN_GROUP']
+CHECK_ALARM= ENV['CHECK_ALARM']
 REGIONS = ENV['REGIONS'].split(',')
 
 def lambda_handler(event:, context:)
@@ -14,6 +16,20 @@ def lambda_handler(event:, context:)
   group = resp.auto_scaling_groups[0]
   diff = group.desired_capacity - group.min_size
   desired_capacity = group.desired_capacity
+
+  # check alarm state
+  if diff > 0
+    cloudwatch = Aws::CloudWatch::Client.new({ region: MAIN_REGION })
+    resp = cloudwatch.describe_alarms({
+      alarm_names: [CHECK_ALARM],
+      state_value: "OK",
+      max_records: 1
+    })
+    if resp.metric_alarms.length > 0
+      puts "Reset capacity to min."
+      diff = 0
+    end
+  end
 
   REGIONS.each do |region|
     autoscaling = Aws::AutoScaling::Client.new({ region: region })
