e8a5bac24b806005a4643f6786c065456efe7227
Hisaaki Sato <hisaaki.sato@speee.jp>
2019-05-07 18:29:08 +0900

    Monitor file descriptor usage

--- web/src/main/java/serposcope/services/UsageCheckService.java
+++ web/src/main/java/serposcope/services/UsageCheckService.java
@@ -7,6 +7,9 @@
  */
 package serposcope.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.time.DayOfWeek;
@@ -48,8 +51,38 @@ public class UsageCheckService {
 
 	MemoryMXBean mxbean;
 
+
+	private String pid = null;
+	private Integer maxOpenFiles;
+
 	public UsageCheckService() {
 		mxbean = ManagementFactory.getMemoryMXBean();
+
+		String vmName = ManagementFactory.getRuntimeMXBean().getName();
+		int p = vmName.indexOf("@");
+		String pid = vmName.substring(0, p);
+		Process process = null;
+		try {
+			process = Runtime.getRuntime()
+					.exec(new String[] { "cat", String.format("/proc/%s/limits", pid) });
+			if (process.waitFor() == 0) {
+				try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+					String line;
+					while ((line = br.readLine()) != null) {
+						if (line.contains("open")) {
+							maxOpenFiles = Integer.parseInt(line.split("\\s+")[4]);
+							this.pid = pid;
+							break;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+		} finally {
+			if (process != null) {
+				process.destroy();
+			}
+		}
 	}
 
 	@Schedule(delay = 1, initialDelay = 0, timeUnit = TimeUnit.MINUTES)
@@ -68,6 +101,35 @@ public class UsageCheckService {
 				+ "used: {} committed: {} usage: {}",
 				nonHeapUsed, nonHeapComitted,
 				String.format("%.2f", nonHeapUsed * 100.0 / nonHeapComitted));
+	}
+
+	@Schedule(delay = 1, initialDelay = 0, timeUnit = TimeUnit.MINUTES)
+	public void checkFileDescriptorUsage() {
+        
+		if (this.pid == null) {
+			return;
+		}
+
+		Process process = null;
+		try {
+			process = Runtime.getRuntime()
+					.exec(new String[] { "/bin/sh", "-c", String.format("ls /proc/%s/fd | wc -l", this.pid) });
+			if (process.waitFor() == 0) {
+				try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+		        	int used = Integer.parseInt(br.readLine());
+		    		LOG.info("[FileDescriptor Usage] "
+		    				+ "used: {} max: {} usage: {}",
+		    				used, this.maxOpenFiles,
+		    				String.format("%.2f", used * 100.0 / this.maxOpenFiles));
+				}
+			}
+		} catch (Exception e) {
+		} finally {
+			if (process != null) {
+				process.destroy();
+			}
+		}
+
 	}
 
 	@Schedule(delay = 10, initialDelay = 60, timeUnit = TimeUnit.SECONDS)
